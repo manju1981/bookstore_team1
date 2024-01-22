f1059492637cfe14c9b17fb60aae67edc65a27bf
hariharan.d <dhariharan89@gmail.com>
2024-01-22 14:34:12 +0530

    added migration for inventory, cart and orders table along with seed for inventory

---
+++ src/main/resources/db/migration/schema/V6__Create_orders.sql
@@ -0,0 +1,12 @@
+create table orders
+(
+    id int not null,
+    book_id INT not null,
+    quantity int not null default 1,
+    total_amount int not null,
+    created_at TIMESTAMP DEFAULT now(),
+    primary key (id),
+    CONSTRAINT fk_books
+        FOREIGN KEY(book_id)
+            REFERENCES books(id)
+)
