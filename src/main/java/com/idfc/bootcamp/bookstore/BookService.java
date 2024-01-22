8f02df6a06c3af51e1bb30cb6202a5423269bbdd
Aman Mandaokar <aman.mandaokar@idfcbank.com>
2024-01-22 12:47:54 +0530

    BE-2 : Bhagyashri/Aman : Create search API

--- src/main/java/com/idfc/bootcamp/bookstore/BookService.java
+++ src/main/java/com/idfc/bootcamp/bookstore/BookService.java
@@ -1,10 +1,11 @@
 package com.idfc.bootcamp.bookstore;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class BookService {
@@ -14,7 +15,10 @@ public class BookService {
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-    public List<Book> fetchAllBooks() {
+    public List<Book> fetchBooks(String search) {
+        if(!Strings.isBlank(search)) {
+            return bookRepository.findByTitleOrAuthorOrDescription(search, search, search);
+        }
         return bookRepository.findAll();
     }
 }
