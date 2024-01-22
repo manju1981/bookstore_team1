09f54411318a20459e0f2f2e27733a199f700e24
Aman Mandaokar <aman.mandaokar@idfcbank.com>
2024-01-22 12:47:54 +0530

    BE-2 : Bhagyashri/Aman : Create search API

--- src/main/java/com/idfc/bootcamp/bookstore/BookController.java
+++ src/main/java/com/idfc/bootcamp/bookstore/BookController.java
@@ -2,9 +2,9 @@ package com.idfc.bootcamp.bookstore;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,7 +17,7 @@ public class BookController {
 
     @CrossOrigin()
     @GetMapping("books")
-    public List<Book> listBooks(){
-        return bookService.fetchAllBooks();
+    public List<Book> listBooks(@RequestParam(required=false) String search){
+        return bookService.fetchBooks(search);
     }
 }
--- src/test/java/com/idfc/bootcamp/bookstore/BookAPiIntegrationTest.java
+++ src/test/java/com/idfc/bootcamp/bookstore/BookAPiIntegrationTest.java
@@ -50,6 +50,49 @@ public class BookAPiIntegrationTest {
         assertEquals(2, books.size());
     }
 
+    @Test
+    @DisplayName("should search in title, description or author when search query is passed")
+    void shouldSearchInTitleDescriptionOrAuthorWhenSearchQueryIsPassed() {
+        Book book1 = new Book("Refactoring", "Author1","description", 2.0);
+        Book book2 = new Book("TDD", "Author2","description", 2.0);
+
+        bookRepository.saveAll(Arrays.asList(book1, book2));
+
+        final List<Book> books = restTemplate.exchange(baseUrl+"/books?search={search}", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Book>>(){}, "Author1").getBody() ;
+
+        assertEquals(1, books.size());
+    }
+
+    @Test
+    @DisplayName("should return three books based on search query")
+    void shouldReturnThreeBooksBasedOnSearchQuery() {
+        Book book1 = new Book("Refactoring", "Author1","test", 2.0);
+        Book book2 = new Book("TDD", "test","description", 2.0);
+        Book book3 = new Book("test", "Author3","description", 2.0);
+        Book book4 = new Book("BOOK4", "Author4","description", 2.0);
+
+        bookRepository.saveAll(Arrays.asList(book1, book2, book3, book4));
+
+        final List<Book> books = restTemplate.exchange(baseUrl+"/books?search={search}", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Book>>(){}, "test").getBody() ;
+
+        assertEquals(3, books.size());
+    }
+
+    @Test
+    @DisplayName("should return empty list when no books match search param")
+    void shouldReturnEmptyListWhenNoBooksMatchSearchParam() {
+        Book book1 = new Book("Refactoring", "Author1","description", 2.0);
+        Book book2 = new Book("TDD", "Author2","description", 2.0);
+
+        bookRepository.saveAll(Arrays.asList(book1, book2));
+
+        final List<Book> books = restTemplate.exchange(baseUrl+"/books?search={search}", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Book>>(){}, "test").getBody() ;
+
+        assertEquals(0, books.size());
+    }
     @AfterEach
     void tearDown() {
         bookRepository.deleteAll();
