58a4752d6f3a976131448632bb1ef7a59cea2b78
Vaibhav Srivastava <vaibhav.srivastava1@idfcbank.com>
2024-01-22 14:54:08 +0530

    Enhancements in Listing Table

---
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookListing/ColumnConfig.js
@@ -0,0 +1,17 @@
+const config = [
+    { field: 'bookName', headerName: 'Book', width: 130 },
+    { field: 'authorName', headerName: 'Author', width: 130 },
+    {
+      field: 'price',
+      headerName: 'Price',
+      type: 'number',
+      width: 150,
+    },
+    {
+      field: 'rating',
+      headerName: 'Rating',
+      width: 130,
+    },
+  ];
+
+  export default config;
