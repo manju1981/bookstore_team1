e3bbb966eb688458e7ab3a702875ca89715a2955
hariharan.d <dhariharan89@gmail.com>
2024-01-22 14:34:12 +0530

    added migration for inventory, cart and orders table along with seed for inventory

---
+++ src/main/resources/db/migration/seed/V7__Inventory_Seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO inventory (book_id,price,stock) VALUES ('1','100','10');
+INSERT INTO inventory (book_id,price,stock) VALUES ('2','200','20');
+INSERT INTO inventory (book_id,price,stock) VALUES ('3','300','30');
+INSERT INTO inventory (book_id,price,stock) VALUES ('4','400','40');
