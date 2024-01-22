4269cb5faa6c46027d0be009748e49db96c6245a
Sanjay Pandey <sanjay.pandey1@idfcbank.com>
2024-01-22 17:12:21 +0530

    add linting

--- src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.test.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.test.js
@@ -1,30 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import BookListing from './BookListing';
+import { render, screen } from '@testing-library/react'
+import BookListing from './BookListing'
 
 test('renders header', () => {
-  render(<BookListing/>);
-  const header = screen.getByTestId("header-bar");
+  render(<BookListing />)
+  const header = screen.getByTestId('header-bar')
   const headerTitle = screen.getByTestId('header-bar-title')
-  expect(header).toBeInTheDocument();
+  expect(header).toBeInTheDocument()
   expect(headerTitle).toHaveTextContent('Team 1 Book Store')
-});
+})
 
 test('it should show table header and 5 rows of books in the list', () => {
-  render(<BookListing/>);
-  const table = screen.getByTestId("list-table");
-  expect(table).toBeInTheDocument();
-  const book = screen.getByText('Book');
-  expect(book).toBeInTheDocument();
-  const author = screen.getByText('Author');
-  expect(author).toBeInTheDocument();
-  const price = screen.getByText('Price');
-  expect(price).toBeInTheDocument();
-  
+  render(<BookListing />)
+  const table = screen.getByTestId('list-table')
+  expect(table).toBeInTheDocument()
+  const book = screen.getByText('Book')
+  expect(book).toBeInTheDocument()
+  const author = screen.getByText('Author')
+  expect(author).toBeInTheDocument()
+  const price = screen.getByText('Price')
+  expect(price).toBeInTheDocument()
+
   // const rating = screen.getByText('Rating');
   // expect(rating).toBeInTheDocument();
   // console.log("Rating--->",rating)
-  const rowList = screen.getAllByRole("row");
-  expect(rowList.length).toBe(4);
-});
-
-
+  const rowList = screen.getAllByRole('row')
+  expect(rowList.length).toBe(4)
+})
