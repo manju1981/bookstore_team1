2121448c3811b67f2da8a561a23906cfb9223d23
Vaibhav Srivastava <vaibhav.srivastava1@idfcbank.com>
2024-01-22 14:54:08 +0530

    Enhancements in Listing Table

--- src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.js
@@ -2,55 +2,37 @@ import { useEffect, useState } from "react"
 import Button from '@mui/material/Button';
 import Header from '../../components/Header'
 import { DataGrid } from '@mui/x-data-grid';
+import config from "./ColumnConfig";
 
 
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70, testid:'header-id' },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, authorName: 'Snow',       bookName: 'Jon',        price: 35,  rating: 4},
+  { id: 2, authorName: 'Lannister',  bookName: 'Cersei',     price: 42,  rating: 4},
+  { id: 3, authorName: 'Lannister',  bookName: 'Jaime',      price: 45,  rating: 4},
+  { id: 4, authorName: 'Stark',      bookName: 'Arya',       price: 16,  rating: 4},
+  { id: 5, authorName: 'Targaryen',  bookName: 'Daenerys',   price: 10,  rating: 4 },
+  { id: 6, authorName: 'Melisandre', bookName: 'Bhagyashri', price: 150, rating: 4 },
+  { id: 7, authorName: 'Clifford',   bookName: 'Ferrara',    price: 44,  rating: 4 },
+  { id: 8, authorName: 'Frances',    bookName: 'Rossini',    price: 36,  rating: 4 },
+  { id: 9, authorName: 'Roxie',      bookName: 'Harvey',     price: 65,  rating: 4 },
 ];
+
 
 
 function DataTable() {
   return (
-    <div style={{ height: 600, width: '100%', justifyContent: 'center', justifySelf: 'center' }}>
+    <div style={{ height: 600, width: '100%' }}>
       <DataGrid
-        data-testid="list-table"
         rows={rows}
-        columns={columns}
+        columns={config}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 5 },
           },
         }}
+        sx={{ gap: 2 }}
         pageSizeOptions={[5, 10]}
         // checkboxSelection
       />
