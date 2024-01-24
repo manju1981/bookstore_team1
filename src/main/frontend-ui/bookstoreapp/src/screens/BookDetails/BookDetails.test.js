e5460be19e8e11bd748bad0425f19ba0dbf6e66c
Pratham Wanmode <prathamwanmode3012@gmail.com>
2024-01-24 10:49:42 +0530

    Book Details Page Initial Commit

---
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import BookDetails from './BookDetails';
+
+test('renders header', () => {
+  render(<BookDetails />);
+  const header = screen.getByTestId("header-bar");
+  const headerTitle = screen.getByTestId('header-bar-title')
+  expect(header).toBeInTheDocument();
+  expect(headerTitle).toHaveTextContent('Book Details')
+});
+
+
+test('render title of the Book', () => {
+  render(<BookDetails />);
+  const bookTitle = screen.getByTestId("book-title");
+  expect(bookTitle).toHaveTextContent('Sample Book Title')
+});
+
+test('render description of the Book', () => {
+  render(<BookDetails />);
+  const bookTitle = screen.getByTestId("book-title");
+  expect(bookTitle).toHaveTextContent('Sample Book Title')
+});
