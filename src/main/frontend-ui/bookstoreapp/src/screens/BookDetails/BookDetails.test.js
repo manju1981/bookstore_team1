0e8aca5ae89a77916ffa4f5704780a5a8e944bcf
Sanjay Pandey <sanjay.pandey1@idfcbank.com>
2024-01-24 10:52:13 +0530

    fix linting

--- src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.test.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.test.js
@@ -1,23 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import BookDetails from './BookDetails';
+import { render, screen } from '@testing-library/react'
+import BookDetails from './BookDetails'
 
 test('renders header', () => {
-  render(<BookDetails />);
-  const header = screen.getByTestId("header-bar");
+  render(<BookDetails />)
+  const header = screen.getByTestId('header-bar')
   const headerTitle = screen.getByTestId('header-bar-title')
-  expect(header).toBeInTheDocument();
+  expect(header).toBeInTheDocument()
   expect(headerTitle).toHaveTextContent('Book Details')
-});
-
+})
 
 test('render title of the Book', () => {
-  render(<BookDetails />);
-  const bookTitle = screen.getByTestId("book-title");
+  render(<BookDetails />)
+  const bookTitle = screen.getByTestId('book-title')
   expect(bookTitle).toHaveTextContent('Sample Book Title')
-});
+})
 
 test('render description of the Book', () => {
-  render(<BookDetails />);
-  const bookTitle = screen.getByTestId("book-title");
+  render(<BookDetails />)
+  const bookTitle = screen.getByTestId('book-title')
   expect(bookTitle).toHaveTextContent('Sample Book Title')
-});
+})
