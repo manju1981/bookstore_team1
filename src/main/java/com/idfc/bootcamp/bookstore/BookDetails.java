b5a106b601bb67983528d4ee1bfaaf9b9776d413
Hariharan <dhariharan89@gmail.com>
2024-01-24 13:02:38 +0530

    unused code removed

--- src/main/java/com/idfc/bootcamp/bookstore/BookDetails.java
+++ src/main/java/com/idfc/bootcamp/bookstore/BookDetails.java
@@ -1,7 +1,5 @@
 package com.idfc.bootcamp.bookstore;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -12,14 +10,10 @@ public class BookDetails {
     private String title;
     private String author;
     private  String description;
-    @JsonSetter(nulls = Nulls.SKIP)
-    private double ratings = 0.0;
-    @JsonSetter(nulls = Nulls.SKIP)
-    private int price = 0;
-    @JsonSetter(nulls = Nulls.SKIP)
-    private int stock = 0;
-    @JsonSetter(nulls = Nulls.AS_EMPTY)
-    private int quantity = 0;
+    private double ratings;
+    private int price ;
+    private int stock ;
+    private int quantity;
 
     public BookDetails(long id, String title, String author, String description, double ratings, int price, int stock, int quantity) {
         this.id = id;
@@ -66,42 +60,6 @@ public class BookDetails {
 
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setRatings(double ratings) {
-        this.ratings = ratings;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
 
