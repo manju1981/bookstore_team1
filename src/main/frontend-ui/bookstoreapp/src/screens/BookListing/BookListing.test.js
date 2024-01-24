684d956658098f19d0bd1a2d17ad91cd13a0fe16
Vaibhav Srivastava <vaibhav.srivastava1@idfcbank.com>
2024-01-24 12:02:59 +0530

    Added Tests

--- src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.test.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.test.js
@@ -1,18 +1,15 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, act, fireEvent } from '@testing-library/react'
 import BookListing from './BookListing'
 import DataTable from './DataTable'
 
 const books = [
-  { id: 1, author: 'Snow', title: 'Jon', price: 35, ratings: 4 },
-  { id: 2, author: 'Lannister', title: 'Cersei', price: 42, ratings: 4 },
-  { id: 3, author: 'Lannister', title: 'Jaime', price: 45, ratings: 4 },
-  { id: 4, author: 'Stark', title: 'Arya', price: 16, ratings: 4 },
-  { id: 5, author: 'Targaryen', title: 'Daenerys', price: 10, ratings: 4 },
-  { id: 6, author: 'Melisandre', title: 'Bhagyashri', price: 150, ratings: 4 },
-  { id: 7, author: 'Clifford', title: 'Ferrara', price: 44, ratings: 4 },
-  { id: 8, author: 'Frances', title: 'Rossini', price: 36, ratings: 4 },
-  { id: 9, author: 'Roxie', title: 'Harvey', price: 65, ratings: 4 },
-]
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+];
+global.fetch = jest.fn().mockResolvedValue({
+  json: jest.fn().mockResolvedValue(mockBooks),
+});
+
 
 test('renders header', () => {
   render(<BookListing />)
@@ -22,7 +19,20 @@ test('renders header', () => {
   expect(headerTitle).toHaveTextContent('Team 1 Book Store')
 })
 
+
+test('handleSearch updates searchString correctly', () => {
+  render(<BookListing />);
+
+  const searchInput = screen.getByRole('textbox', { name: /search/i });
+  act(() => {
+    fireEvent.change(searchInput, { target: { value: 'Code' } });
+  });
+  const searchString = screen.getByRole('textbox', { name: /search/i }).value;
+  expect(searchString).toBe('Code');
+});
+
 test('it should show table header and 5 rows of books in the list', () => {
+
   render(<DataTable books={books} />)
   const table = screen.getByTestId('list-table')
   expect(table).toBeInTheDocument()
@@ -37,4 +47,21 @@ test('it should show table header and 5 rows of books in the list', () => {
   // console.log("Rating--->",rating)
   // const tablerow = screen.getAllByRole('rowgroup')[1];
   // console.log('TableRow', tablerow);
+})
+
+
+test('it should navigate to the details page on click of a row', () => {
+  global.fetch = jest.fn().mockResolvedValue({
+    json: jest.fn().mockResolvedValue(books),
+  });
+
+  render(<DataTable books={books} />)
+  const table = screen.getByTestId('list-table')
+  expect(table).toBeInTheDocument()
+  const book = screen.getByText('Book Title')
+  expect(book).toBeInTheDocument()
+  const author = screen.getByText('Author Name')
+  expect(author).toBeInTheDocument()
+  const price = screen.getByText('Price')
+  expect(price).toBeInTheDocument()
 })
