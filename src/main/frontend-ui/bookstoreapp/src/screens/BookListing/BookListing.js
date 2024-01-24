e47682218114ad4675f3dfe8ff62dff45dcaef0f
Sanjay Pandey <sanjay.pandey1@idfcbank.com>
2024-01-24 11:05:15 +0530

    implement api search

--- src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookListing/BookListing.js
@@ -3,25 +3,16 @@ import DataTable from './DataTable'
 import { useState } from 'react'
 
 const BookListing = () => {
-  const [filteredBooks, setFilteredBooks] = useState([])
+  const [searchString, setSearchString] = useState('')
 
   const handleSearch = (searchValue) => {
-    fetch(`http://localhost:8090/books?search=${searchValue}`)
-      .then((response) => response.json())
-      .then((response) => {
-        const filteredBooks = response.filter(
-          (item) =>
-            item.title.toLowerCase().includes(searchValue.toLowerCase()) ||
-            item.author.toLowerCase().includes(searchValue.toLowerCase())
-        )
-        setFilteredBooks(filteredBooks)
-      })
+    setSearchString(searchValue)
   }
 
   return (
     <>
       <Header title="Team 1 Book Store" onSearch={handleSearch} />
-      <DataTable books={filteredBooks} />
+      <DataTable searchString={searchString} />
     </>
   )
 }
