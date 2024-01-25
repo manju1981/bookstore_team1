f8f9254964a5b7f305d161e0df30483b530b7d64
Pratham Wanmode <prathamwanmode3012@gmail.com>
2024-01-25 12:03:04 +0530

    Integrated Details page with backend
    Fixed merge with main

--- src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.js
@@ -1,31 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import { Grid, Box, Rating } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Grid, Box, Rating, Button, Stack } from '@mui/material'
 import Header from '../../components/Header'
 import { Typography } from '@mui/material'
-import { Card, CardMedia, CardActionArea, Button, Stack } from '@mui/material'
 import BookImage from '../../assets/book-image.png'
+import { useParams } from 'react-router-dom'
 import { Unstable_NumberInput as BaseNumberInput } from '@mui/base/Unstable_NumberInput'
-import { StyledInputRoot, StyledInput, StyledButton } from './BookDetails.style'
-import Star from '@mui/icons-material/Star'
+import {
+  StyledInputRoot,
+  StyledInput,
+  StyledButton,
+} from '../BookDetails/BookDetails.style'
 import RemoveIcon from '@mui/icons-material/Remove'
 import AddIcon from '@mui/icons-material/Add'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 
 const BookDetails = () => {
-  const [book, setBook] = useState(null)
-  useEffect(() => {
-    const fetchBookDetails = async () => {
-      try {
-        const response = await fetch('YOUR_API_ENDPOINT')
-        const data = await response.json()
-        setBook(data)
-      } catch (error) {
-        console.error('Error fetching book details:', error)
-      }
-    }
-
-    fetchBookDetails()
-  }, [])
+  const { id } = useParams()
+  const [bookDetails, setBookDetails] = useState()
 
   const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
     return (
@@ -51,40 +42,42 @@ const BookDetails = () => {
     )
   })
 
-  const labels = {
-    0.5: 'Useless',
-    1: 'Useless+',
-    1.5: 'Poor',
-    2: 'Poor+',
-    2.5: 'Ok',
-    3: 'Ok+',
-    3.5: 'Good',
-    4: 'Good+',
-    4.5: 'Excellent',
-    5: 'Excellent+',
-  }
+  useEffect(() => {
+    const fetcher = () => {
+      fetch(`http://localhost:8090/book/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setBookDetails(data)
+        })
+    }
+    fetcher()
+  }, [id])
+
+  if (!bookDetails) return null
 
   return (
     <>
-      <Header title="Book Details" />
-
+      <Header title="Book Details" data-testid="header-bar" />
       <Grid container paddingLeft={10} paddingTop={10}>
         <Grid item xs={4} maxWidth={20}>
-          <Card>
-            <CardActionArea>
-              <CardMedia component="img" image={book.image} alt="book-image" />
-            </CardActionArea>
-          </Card>
+          <Box
+            component="img"
+            sx={{ width: 300 }}
+            alt={bookDetails.title}
+            src={BookImage}
+          />
         </Grid>
         <Grid item xs={8} textAlign="left" paddingLeft={10}>
           <Typography variant="h5" data-testid="book-title">
-            {book.title}
+            <p>{`${bookDetails.title}`}</p>
           </Typography>
-          <Typography variant="h5" data-testid="book-description">
-            {book.description}
+          <Typography variant="subtitle1" data-testid="book-description">
+            <p>{`${bookDetails.description}`}</p>
           </Typography>
           <Typography variant="h6" data-testid="book-price">
-            {'MRP: ₹'} {book.price}
+            <p>
+              {'MRP: ₹'} {`${bookDetails.price}`}
+            </p>
           </Typography>
           <Box
             sx={{
@@ -94,15 +87,12 @@ const BookDetails = () => {
             }}
           >
             <Rating
-              key={book.id}
               name="text-feedback"
-              value={book.rating}
               readOnly
+              value={bookDetails.ratings}
               data-testid="book-rating"
               precision={0.5}
-              emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
             />
-            <Box sx={{ ml: 2 }}>{labels[book.rating]}</Box>
           </Box>
           <Grid container spacing={2} paddingTop={5}>
             <Grid item xs={12}>
@@ -113,7 +103,7 @@ const BookDetails = () => {
             </Grid>
             <Grid item xs={12} paddingTop={5}>
               <Stack direction="row" spacing={2}>
-                <Button variant="contained" startIcon={<AddShoppingCartIcon />}>
+                <Button variant="contained" startIcon={<ShoppingCartIcon />}>
                   Add to Cart
                 </Button>
               </Stack>
--- src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.test.js
+++ src/main/frontend-ui/bookstoreapp/src/screens/BookDetails/BookDetails.test.js
@@ -1,22 +1,95 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import BookDetails from './BookDetails'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-test('renders header', () => {
-  render(<BookDetails />)
-  const header = screen.getByTestId('header-bar')
-  const headerTitle = screen.getByTestId('header-bar-title')
-  expect(header).toBeInTheDocument()
-  expect(headerTitle).toHaveTextContent('Book Details')
+jest.mock('node-fetch')
+
+const mockBookDetails = {
+  title: 'Sample Book Title',
+  description: 'Sample Book Description',
+}
+
+test('renders header-bar', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockBookDetails),
+  })
+
+  render(
+    <Router>
+      <BookDetails />
+    </Router>
+  )
+
+  await waitFor(() => {
+    const header = screen.getByTestId('header-bar')
+    expect(header).toBeInTheDocument()
+  })
 })
 
-test('render title of the Book', () => {
-  render(<BookDetails />)
-  const bookTitle = screen.getByTestId('book-title')
-  expect(bookTitle).toHaveTextContent('Sample Book Title')
+test('renders header-title', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockBookDetails),
+  })
+
+  render(
+    <Router>
+      <BookDetails />
+    </Router>
+  )
+
+  await waitFor(() => {
+    const headerTitle = screen.getByTestId('header-bar-title')
+    expect(headerTitle).toHaveTextContent('Book Details')
+  })
 })
 
-test('render description of the Book', () => {
-  render(<BookDetails />)
-  const bookTitle = screen.getByTestId('book-title')
-  expect(bookTitle).toHaveTextContent('Sample Book Title')
+test('render title of the Book', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockBookDetails),
+  })
+
+  render(
+    <Router>
+      <BookDetails />
+    </Router>
+  )
+
+  await waitFor(() => {
+    const bookTitle = screen.getByTestId('book-title')
+    expect(bookTitle).toHaveTextContent('Sample Book Title')
+  })
+})
+
+test('render description of the Book', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockBookDetails),
+  })
+
+  render(
+    <Router>
+      <BookDetails />
+    </Router>
+  )
+
+  await waitFor(() => {
+    const bookDescription = screen.getByTestId('book-description')
+    expect(bookDescription).toHaveTextContent('Sample Book Description')
+  })
+})
+
+test('render price of the Book', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockBookDetails),
+  })
+
+  render(
+    <Router>
+      <BookDetails />
+    </Router>
+  )
+
+  await waitFor(() => {
+    const bookDescription = screen.getByTestId('book-price')
+    expect(bookDescription).toHaveTextContent('MRP: ₹')
+  })
 })
